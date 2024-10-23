d25fba84ad295985b9e70f22e9b955f664352d12
Timon Kisera <timonmarick@gmail.com>
2024-10-23 11:32:48 +0300

    Number Guessing Game

--- #3-Projects/2 - ngg.py
+++ #3-Projects/2 - ngg.py
@@ -23,24 +23,39 @@ Otherwise, flag the input as invalid
 
 #Implementation
 
+#Module to generate random number
 import random
 
+#Initiating loop
 while True:
+    #Check if the user wants to play
     option = str(input("\n\nDo you want to play? (y/n): ")).upper()
 
     if option == "Y":
+        #Generating random number
         num = random.randint(1,30)
-        guess = int(input("\nEnter a number between 1 and 30: "))
 
-        while (guess>0 and guess<=30):
-            if num == guess:
-                print("\nHooraaayyy! Your guess is correct\n")
-                break
+        #Implementing exception handling
+        try:
+            #Capture user's guess
+            guess = int(input("\nEnter a number between 1 and 30: "))
+
+            #Check if guess is in specified range
+            while (guess>0 and guess<=30):
+
+                #Check if guess matches generated number
+                if num == guess:
+                    print("\nHooraaayyy! Your guess is correct\n")
+                    break
+                else:
+                    print("\nWhoopssies! Try again\nThe number was: "+str(num)+"\nYour guess was: "+str(guess)+"\n\n")
+                    break
             else:
-                print("\nWhoopssies! Try again\nThe number was: "+str(num)+"\nYour guess was: "+str(guess)+"\n\n")
-                break
-        else:
-            print("Please enter a number in the specified range")
+                print("Please enter a number in the specified range")
+
+        #Capture the exception
+        except ValueError:
+            print("Please enter a valid number")
     
     elif option == "N":
         print("Thanks for playing!")
