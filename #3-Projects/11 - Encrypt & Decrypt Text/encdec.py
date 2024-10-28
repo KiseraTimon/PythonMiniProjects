fe34a6a2fdfa173957be2a07a71378757bd653ff
Timon Kisera <timonmarick@gmail.com>
2024-10-28 23:32:44 +0300

    Encrypt & Decrypt Text

--- #3-Projects/11 - Encrypt & Decrypt Text/encdec.py
+++ #3-Projects/11 - Encrypt & Decrypt Text/encdec.py
@@ -21,91 +21,127 @@ If no:
 """
 
 #Implementation
+
+#System interaction module
 import os
+#Importing cryptography module
 from cryptography.fernet import Fernet
 
 
 #Start function
 def start():
+    #Initializer set to true
     init = True
+
+    #Running service function
     return main(init)
 
 #Service function
 def main(init):
+
+    #Verify initializer is true
     while init == True:
 
-        #Encrypt/Decrypt
+        #Option input
         service = int(input("\nSelect a service to proceed:\n1. Encrypt text\n2. Decrypt file\n3. Exit\n\n"))
 
+        #Encrypt service
         if service == 1:
             encrypt()
             break
+
+        #Decrypt service
         elif service == 2:
             decrypt()
             break
+
+        #Terminate program
         elif service == 3:
             init = False
             main(init)
             break
+
+        #Repeat loop otherwise
         else:
             print("\nInvalid input. Try again\n")
             continue
         
+    #If initializer is false
+    while init == False:
+        print("\n\n##########")
+
+        #Terminate the program
+        break
 
 #Encryption function
 def encrypt():
-    while True:
-        textinput = str(input("\nEnter text to encrypt:\n"))
 
-        try:
+    #Fetching user input to encrypt
+    textinput = str(input("\nEnter text to encrypt:\n"))
 
-            #Encryption key
-            key = Fernet.generate_key()
-            global f
-            f = Fernet(key)
-            global token
-            token = f.encrypt(b'{textinput}')
+    try:
 
-            #Storing key and token in txt file
-            with open("crypt.txt", "w") as file:
-                file.write(f'Key:\n{key.decode()}\nToken:\n{token.decode()}\n')
-                print("\nKey and token saved successfully")
-                return main(init)
+        #Encryption key
+        key = Fernet.generate_key()
+        f = Fernet(key)
 
-        except Exception as e:
-            print(f'\nAn error has occured:\n{e}\n')
-            retry = str(input("Retry? (y/n) ")).lower()
+        #Encryption token
+        token = f.encrypt(textinput.encode())
 
-            if retry == "n":
-                init = True
-                return main(init)
-                break
-            else:
-                print("Invalid input. The system will retry\n")
-                continue
-            
+        #Storing key and token in txt file
+        with open("crypt.txt", "w") as file:
+            file.write(f'Key:\n{key.decode()}\nToken:\n{token.decode()}\n')
+            print("\nKey and token saved successfully\nData can be decrypted by restarting the program")
 
+            #Return to service page
+            init = True
+            return main(init)
+
+    #In case of exceptions, print the error and return the service page
+    except Exception as e:
+        print(f'\nAn error has occured:\n{e}\n')
+
+        init = True
+        return main(init)
+
+#Decryption function
 def decrypt():
-    while True:
         
+    try:
+        #Note for commencement of decryption
         print(f'\n\nDecrypting procedure starting\n\n')
+
+        #Read key and token value from stored file
+        with open ("crypt.txt", "r") as file:
+            lines = file.readlines()
+            key = lines[1].strip()
+            token = lines[3].strip()
+
+        #Decryption procedure
+        f = Fernet(key.encode())
+        decrypt = f.decrypt(token.encode()).decode()
+
+        #Message on success
+        print(f'Decryption successfull\n\nDecrypted data:\n{decrypt}')
         
-        try:
+        #Clear data from stored file by overwriting
+        print(f'\n\n##########\nFile contents will be cleared immediately\n##########')
+        with open ("crypt.txt", "w") as file:
+            file.write("")
 
-            data = f.decrypt(token)
-            b'{data}'
-            print(data)
+        #User heads-up note
+        print(f'Files encrypted after decryption are saved on exit')
 
-        except Exception as e:
-            print(f'Decryption failed:\n{e}\n')
-            retry = str(input("Retry? (y/n) ")).lower()
+        #Return service page
+        init = True
+        return main(init)
 
-            if retry == "n":
-                init = True
-                main(init)
-                break
-            else:
-                print("Invalid input. The system will retry\n")
-                continue
+    #In case of exceptions, return error message and go to service page
+    except Exception as e:
+        print(f'Decryption failed:\n{e}\n')
+        
+        init = True
+        main(init)
 
+#Start the program
 start()
