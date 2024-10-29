bea21ee5197f798b63f9cbf457412c663c2790e8
Timon Kisera <timonmarick@gmail.com>
2024-10-29 23:15:54 +0300

    Currency Converter

--- #3-Projects/12 - Currency Converter/currency.py
+++ #3-Projects/12 - Currency Converter/currency.py
@@ -17,32 +17,51 @@ Display the amount
 """
 
 #Implementation
-from currencies import Currency
 from currency_converter import CurrencyConverter
 
+#Looping structure
 while True:
+
+    #Instructions
     print (f'Instructions:\n\n1. Enter the currency using 3 letters representing the nation\n2. Enter the amount\n3. Enter the currency you wish to convert to\n\n')
+
+    #Default currency
     default = str(input(f'What is the default currency? ')).upper()
+
+    #Amount to be converted
     amount = float(input(f'\nEnter the amount: '))
+
+    #Desired currency
     desired = str(input(f'\nEnter the currency to be converted to: ')).upper()
 
-    if len(default) != 3:
+    #Verify length is 3 values long
+    if (len(default) != 3) and (len(desired) != 3):
         print(f'\n\n##########\nInvalid input! Try again\n##########')
         continue
+
+    #Otherwise, run conversion
     else:
 
         try:
             #Converter
             c = CurrencyConverter()
+
+            #Store final amount
+            """
+            The convert func takes 3-4 arguements: (amount, default currency, desired currency, date)
+            """
             final = c.convert(amount, default, desired)
 
+            #Conversion message
             print(f'\nConverting {default} {amount} to {desired}...\nThe converted amount is: {final}')
             
-            try_again = str(input("Repeat? (y/n)")).lower()
-            if try_again == "n":
+            #Repeat option
+            repeat = str(input("Repeat? (y/n)")).lower()
+            if repeat == "n":
                 print(f'Thank you for using our converter\nTerminating\n##########\n')
                 break
 
+        #Capturing any errors
         except Exception as e:
             print(f'\nAn error occured:\n{e}\nTerminating\n##########\n')
             continue
